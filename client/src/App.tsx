6f4e3a3a2604e12356e552c5e9ddacadabd99ddc
hearte4 <eladheart@gmail.com>
2026-01-18 20:04:32 +0200

    weight

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WeightChart from "./WeightChart";
-import { useState } from "react";
+// import Home from "./Home"; // <-- commented out
 
 function App() {
-  const [weights] = useState([
-    { date: "2020-08-26", weight: 79.7 },
-    { date: "2020-08-26", weight: 78.7 },
-    { date: "2025-09-11", weight: 79.3 },
-    { date: "2026-01-11", weight: 82.7 },
-    { date: "2026-01-18", weight: 83.4 },
-  ]);
-
   return (
-    <div style={{ width: "900px", height: "600px", margin: "0 auto" }}>
-      <WeightChart dataPoints={weights} />
-    </div>
+    <Router>
+      <Routes>
+        {/* Root route */}
+        {/* <Route path="/" element={<Home />} /> */}
+
+        {/* /weight route */}
+        <Route
+          path="/weight"
+          element={
+            <div style={{ width: "900px", height: "600px", margin: "0 auto" }}>
+              <WeightChart
+                dataPoints={[
+                  { date: "2020-08-26", weight: 79.7 },
+                  { date: "2020-08-26", weight: 78.7 },
+                  { date: "2025-09-11", weight: 79.3 },
+                  { date: "2026-01-11", weight: 82.7 },
+                  { date: "2026-01-18", weight: 83.4 },
+                ]}
+              />
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
