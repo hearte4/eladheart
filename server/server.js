8478ae980d0a82f20d7f4169821de089a8ead6c0
hearte4 <eladheart@gmail.com>
2026-01-19 00:05:21 +0200

    weight

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
 // Catch-all route to serve index.html
-app.get("*", (req, res) => {
+app.get("/.*/", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
